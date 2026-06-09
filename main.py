1701de7473b4d2706efd86b56daf29322ecf4f63
Raquib Reyaz <raquibreyaz111@gmail.com>
2026-06-09 14:22:59 +0530

    feat: add comprehensive resume details including personal info, achievements, projects, skills, and summary for multiple programming languages

--- main.py
+++ main.py
@@ -8,21 +8,22 @@ name = "Raquib_Reyaz"
 yaml_file_name = f"{name}_CV.yaml"
 
 # Load YAML files
-with open("info/basic-info.yaml") as f:
+with open("basic-info.yaml") as f:
     basic_info = yaml.safe_load(f)
 
+resume_version = 'cpp-info'
 jd_specific = {}
 
-with open("info/summary.txt", "r", encoding="utf-8") as f:
+with open(f"{resume_version}/summary.txt", "r", encoding="utf-8") as f:
     summary = f.read()
     if summary and len(summary) > 0:
         jd_specific["summary"] = [summary]
-with open("info/skills.json", "r", encoding="utf-8") as f:
+with open(f"{resume_version}/skills.json", "r", encoding="utf-8") as f:
     jd_specific["Technical Skills"] = json.load(f)
-with open("info/projects.json", "r", encoding="utf-8") as f:
+with open(f"{resume_version}/projects.json", "r", encoding="utf-8") as f:
     jd_specific["projects"] = json.load(f)
-with open("info/activities.json", "r", encoding="utf-8") as f:
-    jd_specific["Activities"] = json.load(f)
+with open(f"{resume_version}/achievements.json", "r", encoding="utf-8") as f:
+    jd_specific["Achievements"] = json.load(f)
 
 basic_info["cv"]["sections"] = {**jd_specific, **basic_info["cv"]["sections"]}
 
